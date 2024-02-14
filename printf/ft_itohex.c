d801e4f6cf4d484379a13e01de741d9530979298
Mordisquitos <mariojm2612@gmail.com>
2024-02-14 20:28:25 +0100

    external fts

--- printf/ft_itohex.c
+++ printf/ft_itohex.c
@@ -1,42 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ptoa.c                                          :+:      :+:    :+:   */
+/*   ft_itohex.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mariojim <mariojim@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/12 23:58:34 by mariojim          #+#    #+#             */
-/*   Updated: 2024/02/14 18:06:48 by mariojim         ###   ########.fr       */
+/*   Created: 2024/02/14 18:37:15 by mariojim          #+#    #+#             */
+/*   Updated: 2024/02/14 19:52:15 by mariojim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "libft.h"
 
-char *ft_ptoa(void *ptr)
+char *ft_itohex(int n)
 {
+	int		len;
+	int		tempn;
+	char	base[17];
 	char	*str;
-	int		i;
 
-	str = (char *)malloc(sizeof(char) * ((sizeof(void *) * 2) + 3));
-	if (!str)
-		return (NULL);
-	str[0] = '0';
-	str[1] = 'x';
-	i = 2;
-	while (i < (sizeof(void *) * 2))
+	len = 1;
+	tempn = n;
+	ft_strlcpy(base, "0123456789ABCDEF", 17);
+	while (tempn / 16)
 	{
-		int shift = ((sizeof(void *) * 2) - 1 - (i - 2)) * 4;
+		tempn /= 16;
+		len++;
 	}
-	str[i] = '\0';
+	str = (char *)malloc(sizeof(char) * len + 1);
+	str[len--] = '\0';
+	while (n)
+	{
+		str[len--] = base[n % 16];
+		if (len == 0)
+			str[len] = base[n / 16];
+		n = n / 16;
+	}	
 	return(str);
-}
-int main()
-{
-	char *ptr;
-	char num = '5';
-	
-	ptr = &num;
-	printf("%p\n",ptr);
-	printf("%s\n", ft_ptoa(ptr));
 }
--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,13 +6,25 @@
 /*   By: mariojim <mariojim@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 14:51:01 by mariojim          #+#    #+#             */
-/*   Updated: 2024/02/12 23:58:12 by mariojim         ###   ########.fr       */
+/*   Updated: 2024/02/14 19:57:12 by mariojim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
 #include <stdarg.h>
+#include <libft.h>
 
+static void print_and_free(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar_fd(str[i], 1);
+		free(str[i]);
+		i++;
+	}
+}
 static void	manage_letter(char const ch, va_list args)
 {
 	char	*str;
@@ -26,13 +38,18 @@ static void	manage_letter(char const ch, va_list args)
 		str = va_arg(args, char *);
 	else if (ch == 'd' || ch == 'i')		
 		str = ft_itoa(va_arg(args, int));
+	else if (ch == 'u')
+		str = ft_itoa(va_arg(args, unsigned int));
 	else if (ch == 'p')
 		str = ft_ptoa(va_arg(args, void *));
+	else if (ch == 'x')
+		str = ft_tolower(ft_itohex(va_arg(args, int)));
+	else if (ch == 'X')
+		str = ft_itohex(va_arg(args, int));
 	if (str == NULL)
 		ft_putchar_fd(c, 1);
 	else
-		while (*str)
-			ft_putchar_fd(*str++, 1);
+		print_and_free()
 }
 
 void	ft_printf(char const *s, ...)
@@ -67,9 +84,10 @@ int main()
 	char Char = 'Z';
 	char *String = "Hola";
 	void *Puntero = NULL;
-	int Digito = 69;
+	int Digito = -69;
 
-	ft_printf("Char: %c\nString: %d\n", Char, String);
-	ft_printf("Puntero: %d\nDigito: %d\n", Puntero, Digito);
+	ft_printf("Char: %c\nString: %s\n", Char, String);
+	ft_printf("Puntero: %p\nDigitos: %d y %i\n", Puntero, Digito, Digito);
+	ft_printf("Unsigned: %u\nLowerHex: %x\n", Digito, Hexnum);
+	ft_printf("UpperHex: %x\nPercentile: %%", Hexnum);
 }
-
